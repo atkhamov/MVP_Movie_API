8e0a8ec789b5f3337d47aea3ed44be87d54d1654
unknown <atkhamov@gmail.com>
2019-06-23 13:30:37 +0600

    working version of RecyclerView within the Fragment

--- app/src/main/java/com/example/mvp_movie_api/MainActivity.java
+++ app/src/main/java/com/example/mvp_movie_api/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.mvp_movie_api;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mvp_movie_api.allMovies.MainPageFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainPageFragment mainPageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainPageFragment = new MainPageFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //ft.add(R.id.fragment_container,fragment).commit();
+
+        fragmentManager.beginTransaction().add(R.id.fragmentContainer, mainPageFragment).commit();
+
     }
 }
